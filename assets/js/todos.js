a1157befa79947bbe64b6df119b859926c93a1c9
Vince <37764603+vshelnutt@users.noreply.github.com>
2018-04-15 20:34:30 -0500

    Update todos.js
    fixed spelling errors in comments

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -3,7 +3,7 @@ $("ul").on("click", "li", function(){
  $(this).toggleClass("completed");
 });
 
-//click X to delete todo
+//click trash icon to delete todo
 $("ul").on("click", "span", function(event){
 	$(this).parent().fadeOut(500, function(){
 		$(this).remove();
@@ -13,10 +13,10 @@ $("ul").on("click", "span", function(event){
 
 $("input[type='text']").keypress(function(event){
 	if(event.which===13){
-		//grad todo text from input
+		//grab todo text from input
      var todoText = $(this).val();
      $(this).val("");
-      //create bew li and add to ul
+      //create new li and add to ul
       $("ul").append("<li><span><i class='fas fa-trash'></i> </span> " + todoText + "</li>")
 	}
 	});
